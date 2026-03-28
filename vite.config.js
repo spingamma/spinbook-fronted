0caabe9a23f5341858a1e9f91970bf3a4d444657
Jhonatanfc94 <jhonatanfc94@gmail.com>
2026-03-27 22:29:33 -0400

    prod verc

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // <-- Esto es lo que importa, o simplemente quita esta línea
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/',
 })
